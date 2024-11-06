f7918a7015a2432c3fbfcc0698bd3b3424d49f8c
Sania Joshi <sajo1826@colorado.edu>
2024-11-06 16:30:57 -0700

    added new index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = require('path');
 const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
 const bodyParser = require('body-parser');
 const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
-const bcrypt = require('bcryptjs'); //  To hash passwords
+const bcrypt = require('bcryptjs'); // To hash passwords
 const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
 app.use(express.static('public')); 
 
@@ -19,9 +19,9 @@ app.use(express.static('public'));
 
 // create `ExpressHandlebars` instance and configure the layouts and partials dir.
 const hbs = handlebars.create({
-  extname: 'hbs',
-  layoutsDir: __dirname + '/src/views/layouts',
-  partialsDir: __dirname + '/src/views/partials',
+extname: 'hbs',
+layoutsDir: __dirname + '/ProjectSourceCode/views/layouts',
+partialsDir: __dirname + '/ProjectSourceCode/views/partials',
 });
 
 console.log(__dirname, path.join(__dirname, 'src', 'views'));
@@ -30,24 +30,24 @@ app.use(express.static(path.join(__dirname, 'resources')));
 
 // database configuration
 const dbConfig = {
-  host: 'db', // the database server
-  port: 5432, // the database port
-  database: process.env.POSTGRES_DB, // the database name
-  user: process.env.POSTGRES_USER, // the user account to connect with
-  password: process.env.POSTGRES_PASSWORD, // the password of the user account
+host: 'db', // the database server
+port: 5432, // the database port
+database: process.env.POSTGRES_DB, // the database name
+user: process.env.POSTGRES_USER, // the user account to connect with
+password: process.env.POSTGRES_PASSWORD, // the password of the user account
 };
 
 const db = pgp(dbConfig);
 
 // test your database
 db.connect()
-  .then(obj => {
-    console.log('Database connection successful'); // you can view this message in the docker compose logs
-    obj.done(); // success, release the connection;
-  })
-  .catch(error => {
-    console.log('ERROR:', error.message || error);
-  });
+.then(obj => {
+console.log('Database connection successful'); // you can view this message in the docker compose logs
+obj.done(); // success, release the connection;
+})
+.catch(error => {
+console.log('ERROR:', error.message || error);
+});
 
 // *****************************************************
 // <!-- Section 3 : App Settings -->
@@ -56,73 +56,73 @@ db.connect()
 // Register `hbs` as our view engine using its bound `engine()` function.
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
-// app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'ProjectSourceCode/views'));
 app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
 
 // initialize session variables
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    saveUninitialized: false,
-    resave: false,
-  })
+session({
+secret: process.env.SESSION_SECRET,
+saveUninitialized: false,
+resave: false,
+})
 );
 
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+bodyParser.urlencoded({
+extended: true,
+})
 );
 
 //API ROUTES
 
 app.get('/', (req, res) =>{
-    res.redirect('/home');
+res.redirect('/login');
 });
 
 app.get('/home', (req, res) =>{
-    res.render('pages/home');
+res.render('pages/home');
 });
 
 app.get('/login', (req, res) =>{
-    res.render('pages/login');
+res.render('pages/login');
 });
 
 app.get('/register', (req, res) =>{
-    res.render('page/register');
+res.render('pages/register');
 });
 
 app.get('/stats', (req, res) =>{
-    res.render('pages/stats');
+res.render('pages/stats');
 })
 
 app.post('/register', (req, res) =>{
-    //register page takes input of first name and last name
+//register page takes input of first name and last name
 
-    //takes input and adds 4 rng numbers
+//takes input and adds 4 rng numbers
 
-    //tests wether or not 2 letters of first name and 2 of last PLUS 4 rng numbers is a unique identikey
+//tests wether or not 2 letters of first name and 2 of last PLUS 4 rng numbers is a unique identikey
 
-    //takes password and hashes it
+//takes password and hashes it
 
-    //creates an account, and sends the page a response of identikey and email (which is just identikey + colorado.edu)
-    //and the password
+//creates an account, and sends the page a response of identikey and email (which is just identikey + colorado.edu)
+//and the password
 
 });
 
 app.post('/login', (req, res) =>{
-    //takes in an identikey
-    //if it exists passes through
+//takes in an identikey
+//if it exists passes through
 
-    //checks if password matches password\
-    //if it exists then redirect to the stats (or home) page)
+//checks if password matches password\
+//if it exists then redirect to the stats (or home) page)
 
-    //if incorrect password, stay on login page but provide response
+//if incorrect password, stay on login page but provide response
 
-    //if incorrect identitikey, 
+//if incorrect identitikey, 
 });
 
 //Server Testing
 app.listen(3000, '0.0.0.0', () => {
-  console.log('Server is running on port 3000');
+console.log('Server is running on port 3000');
 });
