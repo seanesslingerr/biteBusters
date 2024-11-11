f23e90618e1f366d8808d3bafdb25d81389b6474
Boston Abrams <boab2355@colorado.edu>
2024-11-10 20:10:45 -0700

    added core courses

--- ProjectSourceCode/init_data/insert.sql
+++ ProjectSourceCode/init_data/insert.sql
@@ -12,4 +12,18 @@ VALUES
   ('CSCI2400', 'Computer Systems', 'none'),
   ('CSCI3104', 'Algorithms', 'none'),
   ('CSCI3155', 'Principles of Programming Languages', 'none'),
-  ('CSCI3308', 'Software Development Methods and Tools', 'none');
+  ('CSCI3308', 'Software Development Methods and Tools', 'none'),
+  ('CSCI3002', 'Fundamentals of Human Computer Interaction', 'none'),
+  ('CSCI3202', 'Introduction to Artificial Intelligence', 'none'),
+  ('CSCI3287', 'Design & Analysis of Database Systems', 'none'),
+  ('CSCI3302', 'Introduction to Robotics', 'none'),
+  ('CSCI3403', 'Introduction to CyberSecurity for a Converged World', 'none'),
+  ('CSCI3434', 'Theory of Computation', 'none'),
+  ('CSCI3656', 'Numerical Computation', 'none'),
+  ('CSCI3753', 'Design & Analysis of Operating Systems', 'none'),
+  ('CSCI4022', 'Advanced Data Science', 'none'),
+  ('CSCI4273', 'Network Systems', 'none'),
+  ('CSCI4448', 'Object-Oriented Analysis and Design', 'none');
+
+
+-
