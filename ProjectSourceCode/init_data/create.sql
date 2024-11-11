af21591c023279216e3f1b6afebfc77b09e833e3
Boston Abrams <boab2355@colorado.edu>
2024-11-10 19:19:11 -0700

    added classes

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -3,3 +3,10 @@ CREATE TABLE users(
     username VARCHAR(50) PRIMARY KEY,
     password CHAR(60) NOT NULL
 );
+
+CREATE TABLE classes(
+    clcasscode VARCHAR(9) PRIMARY KEY,
+    Name CHAR(60) NOT NULL,
+    description VARCHAR(500)
+);
+
