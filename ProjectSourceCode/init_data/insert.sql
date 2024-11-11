a1e868f893cb8708c7adbc980edb4e9fd69bd506
Boston Abrams <boab2355@colorado.edu>
2024-11-10 20:01:33 -0700

    add foundation courses

--- ProjectSourceCode/init_data/insert.sql
+++ ProjectSourceCode/init_data/insert.sql
@@ -2,3 +2,14 @@ INSERT INTO users
   (username, password)
 VALUES
   ('test', '1234');
+
+INSERT INTO classes
+  (class_code, name, description)
+VALUES
+  ('CSCI1000', 'Computer Science as a Field of Work and Study', 'none'),
+  ('CSCI1300', 'Computer Science 1: Starting Computing', 'none'),
+  ('CSCI2270', 'Computer Science 2: Data Structures', 'none'),
+  ('CSCI2400', 'Computer Systems', 'none'),
+  ('CSCI3104', 'Algorithms', 'none'),
+  ('CSCI3155', 'Principles of Programming Languages', 'none'),
+  ('CSCI3308', 'Software Development Methods and Tools', 'none');
