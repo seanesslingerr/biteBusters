653e3bb10f377d08b27b397ae9a8a90eaaf109bd
Boston Abrams <boab2355@colorado.edu>
2024-11-10 19:38:31 -0700

    added major_to_classes

--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -11,14 +11,19 @@ CREATE TABLE classes(
 );
 
 CREATE TABLE users_to_classes(
-    
     username VARCHAR(50) PRIMARY KEY,
     class_code VARCHAR(9) NOT NULL,
     grade CHAR(1),
     semester VARCHAR(4),
     FOREIGN KEY (username) REFERENCES users (username) ON DELETE CASCADE,
     FOREIGN KEY (class_code) REFERENCES classes (class_code) ON DELETE CASCADE
+);
 
-
+CREATE TABLE major_to_classes(
+    major VARCHAR(50) PRIMARY KEY,
+    class_code VARCHAR(9) NOT NULL,
+    requirment_met  CHAR(1) NOT NULL,
+    FOREIGN KEY (major) REFERENCES majors (major) ON DELETE CASCADE,
+    FOREIGN KEY (class_code) REFERENCES classes (class_code) ON DELETE CASCADE
 );
 
