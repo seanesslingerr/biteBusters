ebc35048d6c378e51df4336b51105db935eac23b
Boston Abrams <boab2355@colorado.edu>
2024-11-28 22:10:23 -0700

    cannot get async stuff to work right

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -13,104 +13,79 @@ const bcrypt = require('bcryptjs'); // To hash passwords
 const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
 app.use(express.static('public'));
 var formidable = require('formidable');
-let semester = new Array(75);
-let number = new Array(75);
-let credits = new Array(75);
-let grade = new Array(75);
 
 function scraper(loc) {
+  const semester = new Array(75);
+  const number = new Array(75);
+  const credits = new Array(75);
+  const grade = new Array(75);
 
-  // Create Varables to store the information scraped.
-
-
-
-// Reading in the file -- currently hardcoded to be my audit
-const fs = require('node:fs');
-fs.readFile(loc, 'utf8', (err, data) => {
-
-  // Error Handliing
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  // Define Varables
-  let line = ""
-  let lineNo = 1
-  let leftRows = 5;
-  let fill = 0
-  const usefullRows = ["", "", "", ""];
-
-
-  for (let i = 0; i < data.length; i++) {
-
-    // If the line terminator is not found than keep adding chars to the line
-    if (data[i] !== "\n") {
-        line = line + data[i];
-
+  const fs = require('node:fs');
+  reader = fs.readFile(loc, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
     }
-    // if you do find the line terminator then you are at the end of a line, so do
-    else {
-      
-      //if (lineNo === 1032) { {{ FINDING THE LINE }}
-      //  console.log("on");        
-      //  console.log(line);
-      //}
+    // Define Varables
+    let line = ""
+    let lineNo = 1
+    let leftRows = 5;
+    let fill = 0
+    const usefullRows = ["", "", "", ""];
+    for (let i = 0; i < data.length; i++) {
 
-      // If we should be reaing the data in, triggered by other if statment below
-      if (leftRows < 5) {
-
-        //Fill the usefull rows array with the usefull lines
-        usefullRows[4-leftRows] = line;
-        leftRows = leftRows - 1;
-
-        // If the whole set of usefull for this indivdual class has been looked through
-        if(leftRows === 0) {
-          leftRows = 5;
-          //console.log(usefullRows[0]);
-
-          // Fill the arrays and console log for debug
-          semester[fill] = usefullRows[0].slice(54,58);
-          number[fill] = usefullRows[1].slice(58,66);
-          credits[fill] = usefullRows[2].slice(59,62);
-          grade[fill] = usefullRows[3].slice(56,57);
-          fill = fill+1;
-          //console.log("found");
-          //console.log(usefullRows[0].slice(54,58));
-          //console.log(usefullRows[1].slice(58,66));
-          //console.log(usefullRows[2].slice(59,62));
-          //console.log(usefullRows[3].slice(56,57));
-          //console.log("");
-
-          usefullRows[0] = "";
-          usefullRows[1] = "";
-          usefullRows[2] = "";
-          usefullRows[3] = "";
-        }
-
+      // If the line terminator is not found than keep adding chars to the line
+      if (data[i] !== "\n") {
+          line = line + data[i];
 
       }
+      // if you do find the line terminator then you are at the end of a line, so do
+      else {
+        
+        // If we should be reaing the data in, triggered by other if statment below
+        if (leftRows < 5) {
+
+          //Fill the usefull rows array with the usefull lines
+          usefullRows[4-leftRows] = line;
+          leftRows = leftRows - 1;
+
+          // If the whole set of usefull for this indivdual class has been looked through
+          if(leftRows === 0) {
+            leftRows = 5;
+            //console.log(usefullRows[0]);
+
+            // Fill the arrays and console log for debug
+            semester[fill] = usefullRows[0].slice(54,58);
+            number[fill] = usefullRows[1].slice(58,66);
+            credits[fill] = usefullRows[2].slice(59,62);
+            grade[fill] = usefullRows[3].slice(56,57);
+            fill = fill+1;
+            
+            usefullRows[0] = "";
+            usefullRows[1] = "";
+            usefullRows[2] = "";
+            usefullRows[3] = "";
+          }
+        }
+      // The line is the line right above the info we need about a class, it start the process of reading in the data
+      if (line === '											<tbody><tr class="takenCourse ">') {
+        leftRows = 4;
+      }
+      // end of loop housekeeping
+      lineNo = lineNo  + 1;
+          line = "";
+      }
+    } 
+  });
+  reader.then(()  => {
+    console.log("in then")
+    return [semester, number, credits, grade];
+  });
   
-    // The line is the line right above the info we need about a class, it start the process of reading in the data
-    if (line === '											<tbody><tr class="takenCourse ">') {
-      leftRows = 4;
-    }
-
-    // end of loop housekeeping
-    lineNo = lineNo  + 1;
-        line = "";
-    }
-  } 
-
-
-});
-console.log(semester);
-console.log(number);
-console.log(credits);
-console.log(grade);
-return [semester, number, credits, grade];
 
 }
+
+
 const student_courses = `
   SELECT DISTINCT
     classes.class_code,
@@ -371,20 +346,22 @@ app.post('/fileupload', async (req, res) => {
   
   console.log("test123")
   var form = new formidable.IncomingForm();
-
+  var outputofscraper = [ "bad", "Bad", "bad", "bad"]
   form.parse(req, function (err, fields, files) {
+    console.log(files.filetoupload[0].filepath);
     outputofscraper = scraper(files.filetoupload[0].filepath);
-    console.log(outputofscraper);
-    console.log(semester);
-    console.log(number);
-    console.log(credits);
-    console.log(grade);
+    
+
+
+    //const update = db.any("INSERT INTO users_to_classes (username, class_code, grade, semester) VALUES ($1, $2, $3, $4);", [req.session.user.username, number[0], grade[0], semester[0]]);
+
+
     // Loosely inspired by: https://www.w3schools.com/nodejs/nodejs_uploadfiles.asp
 });
+console.log("start");
+console.log(outputofscraper);
+console.log("end");
 });
-
-
-
 
 //Server Testing
 /*app.listen(3000, '0.0.0.0', () => {
