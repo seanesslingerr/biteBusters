8975169a0fdaddee626db7c92a34a202f52e46fb
Ryan Brace <rybr9677@colorado.edu>
2024-12-02 16:59:04 -0700

    committing for render

--- ProjectSourceCode/init_db.sh
+++ ProjectSourceCode/init_db.sh
@@ -2,7 +2,7 @@
 # DO NOT PUSH THIS FILE TO GITHUB
 # This file contains sensitive information and should be kept private
 # TODO: Set your PostgreSQL URI - Use the External Database URL from the Render dashboard
-PG_URI="postgresql://postgresdb:4c987j8UULBReT5TrZG4z1d0P0GiUTCX@dpg-ct0at29opnds73aalch0-a.oregon-postgres.render.com/audit_db_sw73"
+PG_URI="postgresql://users_db_mh9o_user:wztfPTsjypoezcujR6w7Z2EiyUWrvXFQ@dpg-csv8hg2j1k6c73c2l0fg-a.oregon-postgres.render.com/users_db_mh9o3"
 # Execute each .sql file in the directory
 for file in init_data/*.sql; do
     echo "Executing $file..."
