fdbb800466e5ab5382a6f4f9235a28dd0a1bd074
Ryan Brace <rybr9677@colorado.edu>
2024-12-03 18:48:52 -0700

    changed path

--- ProjectSourceCode/init_db.sh
+++ ProjectSourceCode/init_db.sh
@@ -4,7 +4,9 @@
 # TODO: Set your PostgreSQL URI - Use the External Database URL from the Render dashboard
 PG_URI="postgresql://exampleuser:M0ZifhV2XNYoYfIfe67IRkiYmdySqCJ4@dpg-ct7o4qaj1k6c73ckasj0-a.oregon-postgres.render.com/users_db_hqey"
 # Execute each .sql file in the directory
-for file in init_data/*.sql; do
+for file in ProjectSourceCode/init_data/*.sql; do
     echo "Executing $file..."
     psql $PG_URI -f "$file"
 done
+
+
